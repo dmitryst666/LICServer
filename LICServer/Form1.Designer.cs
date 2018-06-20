803a1845a9da144f121d9b5598e8de39b1f688ac
dmitryst666 <dmitryst666@gmail.com>
2018-06-20 16:40:51 +0400

    Initial

--- LICServer/Form1.Designer.cs
+++ LICServer/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.connDB = new System.Windows.Forms.Button();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.dgvViewer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N/A";
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(12, 12);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(75, 23);
+            this.createBtn.TabIndex = 1;
+            this.createBtn.Text = "CreateDB";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // connDB
+            // 
+            this.connDB.Location = new System.Drawing.Point(93, 12);
+            this.connDB.Name = "connDB";
+            this.connDB.Size = new System.Drawing.Size(100, 23);
+            this.connDB.TabIndex = 2;
+            this.connDB.Text = "Connect to DB";
+            this.connDB.UseVisualStyleBackColor = true;
+            this.connDB.Click += new System.EventHandler(this.connDB_Click);
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(199, 12);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(100, 23);
+            this.readBtn.TabIndex = 3;
+            this.readBtn.Text = "Read All from DB";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // dgvViewer
+            // 
+            this.dgvViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViewer.Location = new System.Drawing.Point(12, 101);
+            this.dgvViewer.Name = "dgvViewer";
+            this.dgvViewer.Size = new System.Drawing.Size(776, 324);
+            this.dgvViewer.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvViewer);
+            this.Controls.Add(this.readBtn);
+            this.Controls.Add(this.connDB);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button connDB;
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.DataGridView dgvViewer;
     }
 }
 
--- LICServer/Form1.cs
+++ LICServer/Form1.cs
@@ -7,14 +7,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
 
 namespace LICServer
 {
+    
+
     public partial class Form1 : Form
     {
+        private string dbFileName;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+
         public Form1()
         {
             InitializeComponent();
+           
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+
+            dbFileName = "sample.sqlite";
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dbFileName))
+                SQLiteConnection.CreateFile(dbFileName);
+
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS Catalog (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, book TEXT)";
+                m_sqlCmd.ExecuteNonQuery();
+
+                label1.Text = "Connected";
+            }
+            catch (SQLiteException ex)
+            {
+                label1.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void connDB_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dbFileName))
+                MessageBox.Show("Please, create DB and blank table (Push \"Create\" button)");
+
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                label1.Text = "Connected";
+            }
+            catch (SQLiteException ex)
+            {
+                label1.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            DataTable dTable = new DataTable();
+            String sqlQuery;
+
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Opening connection with database");
+                connDB.PerformClick();
+                //return;
+            }
+
+            try
+            {
+                sqlQuery = "SELECT * FROM Catalog";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+                adapter.Fill(dTable);
+
+                if (dTable.Rows.Count > 0)
+                {
+                    dgvViewer.Rows.Clear();
+
+                    for (int i = 0; i < dTable.Rows.Count; i++)
+                        dgvViewer.Rows.Add(dTable.Rows[i].ItemArray);
+                }
+                else
+                    MessageBox.Show("Database is empty");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+
+    public class Prod
+    {
+        
+        public int ProductId { get; set; }
+        
+        public string Name { get; set; }
     }
 }
